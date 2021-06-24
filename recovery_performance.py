980bb199ffeaac6f14bafefd5e42314d2bcc7866
ali-vosoughi <64918363+ali-vosoughi@users.noreply.github.com>
2021-06-24 12:23:35 -0400

    Update recovery_performance.py

--- recovery_performance.py
+++ recovery_performance.py
@@ -18,8 +18,7 @@ def recovery_performance(Adjs,label):
     Adjs=Adjs.copy()
     label=label.copy()
     N=len(Adjs)
-    auc_all = np.zeros((N), dtype=np.float32)
-    
+    auc_all = np.zeros((N), dtype=np.float32)   
     for i in range(N):
         auc_all[i] = metrics.roc_auc_score(label[i].flatten(), Adjs[i].flatten())
     return auc_all
