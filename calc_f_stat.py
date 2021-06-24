15bb5399a3b25cd89588a4eb856e165eef709c2d
ali-vosoughi <64918363+ali-vosoughi@users.noreply.github.com>
2021-06-24 12:09:29 -0400

    Update calc_f_stat.py

--- calc_f_stat.py
+++ calc_f_stat.py
@@ -5,8 +5,7 @@ Input: RSS_R,RSS_U,n,pu,pr
 Output: f_GC as values of f-statistic
 
 - "RSS_R" and unrestricted "RSS_U" models.
-- "pu" and "pr" are the number of parameters to be estimated for the unrestricted 
-           and restricted model, respectively. 
+- "pu" and "pr" are the number of parameters to be estimated for the unrestricted and restricted model, respectively. 
 - "n" is the number of time-delayed vectors. 
 
 """
